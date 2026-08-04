9d10dfd630cc0f5169e1b6017f8fe212d02b4c5d
Muhammad Hanzla <sayhi.hanzla@gmail.com>
2026-08-04 17:48:25 +0500

    fix: add connectDB return type

--- apps/api/src/config/db.ts
+++ apps/api/src/config/db.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 import logger from "./logger.js";
 
-export default async function connectDB(uri: string) {
+export default async function connectDB(uri: string): Promise<void> {
   await mongoose.connect(uri);
   logger.info("Database connected successfully");
 }
